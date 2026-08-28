3513587f3dc7b2570e06a946c150560a48ff789f
bperezt0 <bperezt0@email.cpcc.edu>
2026-08-28 13:31:52 -0400

    Revert "HelloWorldProgramModified"
    This reverts commit 998cec3e7964d0b45e4e154f4283ef1e8723a06a.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld
 {
    public static void main(String[] args)
       {
-         System.out.println("Hello world1");
-         System.out.print("Program modified, but able to revert to original version using GitHub Desktop.");
+         System.out.print("Hello world1");
       }
 }
