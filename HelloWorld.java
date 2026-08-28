998cec3e7964d0b45e4e154f4283ef1e8723a06a
bperezt0 <bperezt0@email.cpcc.edu>
2026-08-28 13:27:05 -0400

    HelloWorldProgramModified
    HelloWorld Program modified to add "Program modified, but able to revert to original version using GitHub Desktop.”

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld
 {
    public static void main(String[] args)
       {
-         System.out.print("Hello world1");
+         System.out.println("Hello world1");
+         System.out.print("Program modified, but able to revert to original version using GitHub Desktop.");
       }
 }
